35b81a7857554ca7ce57b0c88c93781abf43c0d2
Zaid Ahmed Khan <126311558+zaidahmedkhan@users.noreply.github.com>
2025-12-02 17:35:55 +0500

    Update conversion_operation.js

--- 01_basics/conversion_operation.js
+++ 01_basics/conversion_operation.js
@@ -29,3 +29,29 @@ console.log(stringNumber);
 console.log(typeof stringNumber);
 
 
+// *********************** Operations ************************888
+
+let value = 8;
+let negValue = -value;
+
+console.log(negValue);
+
+
+let str1 = "zaid";
+let str2 = " ahmed";
+let str3 = " khan";
+
+console.log(str1 + str2 + str3);
+
+
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+gameCounter++;
+
+
+
+
