3c543deafea4a9449cf787360ee58e34333b1bed
Zaid Ahmed Khan <126311558+zaidahmedkhan@users.noreply.github.com>
2025-12-02 13:55:27 +0500

    data types

---
+++ 01_basics/data_types.js
@@ -0,0 +1,19 @@
+"use strict";   // treat all js code as newer version
+
+// alert(3 + 3); // we are usong node js not browser
+
+
+// data types:
+
+// number => 2 to power 53
+// bigInt => for large numbers
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => 
+// symbol => for uniqueness
+
+
+console.log(typeof undefined);
+console.log(typeof null);
+
