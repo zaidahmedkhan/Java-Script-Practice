f81336309aeacd0f1ad2bc8dc3339479e0cda857
Zaid Ahmed Khan <126311558+zaidahmedkhan@users.noreply.github.com>
2025-12-03 14:30:51 +0500

    Create nums_and_math.js

---
+++ 01_basics/nums_and_math.js
@@ -0,0 +1,27 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+console.log(balance.toString().length);
+
+const hundereds = 100000000000;
+
+console.log(hundereds.toLocaleString('en-IN'));
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.0));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+
+
+
+
+
+
+
+
