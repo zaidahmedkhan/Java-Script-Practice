f3f13c5eb7fd4233873c925ae0b9b03ec23c82b0
Zaid Ahmed Khan <126311558+zaidahmedkhan@users.noreply.github.com>
2025-12-08 12:33:39 +0500

    Update arrays.js

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -16,9 +16,39 @@ console.log(myArr.indexOf(3));
 
 
 console.log("A", myArr);
-const myn1 = myArr.slice(1,3);
+const myn1 = myArr.slice(1, 3);
 console.log(myn1);
 console.log("B", myArr);
-console.log(myArr.splice(1,3));
+console.log(myArr.splice(1, 3));
 console.log("c", myArr);
+
+const marvel_heros = ["thor", "Ironman", "spiderman"];
+const dc_heros = ["superman", "flash", "batman"];
+
+marvel_heros.push(dc_heros);
+console.log(marvel_heros);
+console.log(marvel_heros[3][2]);
+
+
+const allHeros = marvel_heros.concat(dc_heros);
+console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros];
+
+console.log(all_new_heros);
+
+const flat_array = [1, 2, 3, [4, 5, 6], 7, 8, [9, 10, [11, 12]]];
+
+console.log(flat_array.flat(Infinity));
+
+
+console.log(Array.isArray("Zaid"));
+console.log(Array.from("Zaid"));
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
+
 
